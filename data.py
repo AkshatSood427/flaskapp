cae0508bca89f58113c6b57ae00e00ba1ed02d98
akshat sood <akshatpaprola@gmail.com>
2024-02-24 13:25:50 +0530

    this is the updated code

--- data.py
+++ data.py
@@ -1,12 +1,48 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, jsonify, request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-@app.route('/')
-def welcome():
-    return render_template('home.html')
+# MySQL Configuration
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'akshat427'
+app.config['MYSQL_DB'] = 'mydatabase'
+
+mysql = MySQL(app)
+
+# API route to insert data into the 'users' table
+@app.route('/api/add_user', methods=['POST'])
+def add_user():
+    try:
+        name = request.json['name']
+        marks = request.json['marks']
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO users (name, marks) VALUES (%s, %s)", (name, marks))
+        mysql.connection.commit()
+        cur.close()
+
+        return jsonify(message='User added successfully')
+
+    except Exception as e:
+        return jsonify(error=str(e))
+
+# API route to retrieve all users from the 'users' table
+@app.route('/api/get_users', methods=['GET'])
+def get_users():
+    try:
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT * FROM mytable")
+        data = cur.fetchall()
+        cur.close()
+
+        return jsonify(users=data)
+
+    except Exception as e:
+        return jsonify(error=str(e))
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
--- hello.py
+++ hello.py
@@ -1,14 +1,10 @@
-from flask import Flask
-from flask import render_template
-from flask import make_response
+from flask import Flask , render_template
+
 app = Flask(__name__)
-@app.route('/')
-def home():
+
+@app.route('/',methods=['GET'])
+def rendfile():
     return render_template('home.html')
-@app.route('/<page_name>')
-def other_page(page_name):
-    response = make_response('The page named %s does not exist.' \
-                             % page_name, 404)
-    return response
+
 if __name__ == '__main__':
     app.run(debug=True)
