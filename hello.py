8b30ac7da46781fe64cfcdbfae3b0609889e2d20
akshat sood <akshatpaprola@gmail.com>
2024-02-25 15:05:43 +0530

    the commit added a new file but i couldnt solve any of my problems
    the templates still dont render and i couldnt test the db connectivity

--- hello.py
+++ hello.py
@@ -1,10 +1,12 @@
-from flask import Flask , render_template
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
+@app.route('/', methods=['GET'])
 def rendfile():
-    return render_template('home.html')
+    template_path = os.path.join(os.path.dirname(__file__), 'templates/home.html')
+    return render_template(template_path)
 
 if __name__ == '__main__':
     app.run(debug=True)
